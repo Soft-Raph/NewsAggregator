1c058dca6a5344b1768372f9f2658f2005ad4949
Ariyo Ayomide <ariyoraphael1@gmail.com>
2025-01-11 01:48:42 +0200

    added the command to everyMinute schedule in route console

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('articles:fetch', function () {
+    $this->info('Fetching articles from news APIs...');
+})->everyMinute();
