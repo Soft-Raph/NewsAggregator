69a1a544725ddd2beb26c962f3b51d4f38f5c1c9
Ariyo Ayomide <ariyoraphael1@gmail.com>
2025-01-11 00:08:28 +0200

    install api route

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $query = Article::query();
+
+        if ($request->filled('search')) {
+            $query->where('title', 'like', '%' . $request->search . '%');
+        }
+
+        if ($request->filled('category')) {
+            $query->where('category', $request->category);
+        }
+
+        if ($request->filled('source')) {
+            $query->where('source', $request->source);
+        }
+
+        if ($request->filled('date')) {
+            $query->whereDate('published_at', $request->date);
+        }
+
+        return response()->json($query->paginate(10));
+    }
 }
